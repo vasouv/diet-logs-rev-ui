86f1bff7a8f8fe0c3dbd38b797bd3799fc3abf99
Vasilis Souvatzis <vasouv@gmail.com>
2022-06-12 18:35:33 +0300

    appointments component
    sample data for appointments component
    closes #4

---
+++ src/app/mock-appointments.ts
@@ -0,0 +1,6 @@
+import {Appointment} from "./appointment";
+
+export const APPOINTMENTS: Appointment[] = [
+  {id: 1, date: new Date().toDateString(), customer: "vasouv"},
+  {id: 2, date: new Date().toDateString(), customer: "aggela"}
+]
